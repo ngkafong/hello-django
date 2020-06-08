72443db0ffd0fd9e236fb4f6d0ea71c025dbb5c4
Ng Ka Fong <richardng1126@gmail.com>
2020-06-08 22:45:31 +0800

    html built-in filter, for loop, if

--- src/pages/views.py
+++ src/pages/views.py
@@ -8,4 +8,15 @@ def home_view(request, *args, **kwargs):
     return render(request, "home.html", {})
 
 def contact_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Contact Page</h1>")
+    return render(request, "contact.html", {})
+
+def about_view(request, *args, **kwargs):
+    my_context = {
+        "my_text": "This is about us",
+        "my_number": 123,
+        "my_list": [1313, 4231, 312, 'Abc'],
+    }
+    return render(request, "about.html", my_context)
+
+def social_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Social Page</h1>")
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from pages.views import home_view
+from pages.views import home_view, about_view, contact_view, social_view
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('home/', home_view, name='home'),
     path('admin/', admin.site.urls),
+    path('about/', about_view, name='about'),
+    path('contact/', contact_view, name='contact'),
+    path('about/', social_view, name='social')
 ]
